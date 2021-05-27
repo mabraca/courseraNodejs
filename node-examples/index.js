73490a88adad711e719569e8cfc15b3bf97a5ff4
Maria Bracamonte <mabraca18@gmail.com>
2021-05-27 15:40:24 -0400

    first example

---
+++ node-examples/index.js
@@ -0,0 +1,21 @@
+var rest = {
+    perimeter : (x,y) => (2*(x+y)),
+    area : (x,y) => (x*y)
+};
+
+function solveRect(l,b){
+    console.log(`Solving for rectangule with l = ${l} and b = ${b}`);
+    if(l <= 0 || b<=0){
+        console.log(`Rectangule dimensions should be greater than zero: l= ${l} and b = ${b}`);
+    }else{
+        console.log("The area of the rectangule is "+ rest.area(l,b));
+        console.log("The perimeter of the rectangule is "+ rest.perimeter(l,b));
+
+    }
+
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
