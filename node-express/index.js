0f0c47980be7992334a5e77448c4167b130b9328
Maria Bracamonte <mabraca18@gmail.com>
2021-05-28 17:13:01 -0400

    server express

---
+++ node-express/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+
+const app = express();
+
+
+// req = request
+// res = response
+// next = middleware
+app.use((req,res,next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    res.end("<html><body><h1>This is an Express server</h1></body></html>");
+});
+
+
+const server = http.createServer(app);
+
+server.listen(port,hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+})
