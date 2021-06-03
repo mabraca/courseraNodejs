e549eb307d043efa4f0a5a3994df3758b43922ac
Maria Bracamonte <mabraca18@gmail.com>
2021-06-03 16:16:31 -0400

    Mongoose part two and three

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -5,20 +5,34 @@ const connect = mongoose.connect(url);
 connect.then((db)=>{
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: "Uthapizza",
         description: "test"
-    });
+    }).then((dish) =>{
 
-    newDish.save().then((dish) =>{
-        
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set: {description : "Update test"}
+        },{
+            new: true
+        }).exec();
 
-    }).then((dishes) => {
+    }).then((dish) => {
 
-        console.log(dishes);
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting  a sinking feeling',
+            author: "Maria Bracamonte"
+        });
+
+        return dish.save();
+
+    }).then((dish)=> {
+       
+        console.log(dish);
 
         return Dishes.remove({});
 
