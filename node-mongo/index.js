f5942a32f65c215acb74602cd02243dc80f30e11
Maria Bracamonte <mabraca18@gmail.com>
2021-06-03 15:57:35 -0400

    Mongoose schema

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,32 +5,45 @@ const dboper = require("./operations");
 const url = "mongodb://localhost: 27017/";
 const dbname = "conFusion";
 
-MongoCliente.connect(url,(err, client)=> {
-    assert.equal(err, null);
+MongoCliente.connect(url).then((client)=> {
+    
     console.log("Connected correctly to the server");
     const db = client.db(dbname);
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes", (result) => {
-            console.log("Insert Document:\n", result.ops);
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},"dishes").then((result) => {
+        
+        console.log("Insert Document:\n", result.ops);
 
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
+        return dboper.findDocuments(db, "dishes");
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
-                            
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection: ", result);
+    }).then((docs) => {
+        
+            console.log("Found Documents:\n", docs);
 
-                                client.close();
-                            });
-                        });
-                    });
-            });
-    });
+            return dboper.updateDocument(db, { name: "Vadonut" },{ description: "Updated Test" }, "dishes")
+
+    }).then((result) => {
+
+            console.log("Updated Document:\n", result.result);
+
+            return dboper.findDocuments(db, "dishes");
+
+    }).then((docs) => {
+
+            console.log("Found Updated Documents:\n", docs);
+            
+            return db.dropCollection("dishes");
+
+    }).then((result) => {
+            
+        console.log("Dropped Collection: ", result);
+
+        client.close();
+            
+    }).catch((error) => {
+        console.log("Error inside : "+ error);
+    })
+
+}).catch((error) => {
+    console.log("Error outside: "+ error);
 });
