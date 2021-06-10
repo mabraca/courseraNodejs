91de3975805ffbc2656ede271764085260c31f11
Maria Bracamonte <mabraca18@gmail.com>
2021-06-10 09:26:22 -0400

    passport

---
+++ conFusionServer/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+
+const userSchema = new Schema({
+    admin: {
+        type : Boolean,
+        default: false
+    }
+},{
+    timestamps: true
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+var Users = mongoose.model('User', userSchema);
+
+module.exports= Users;
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -1,9 +1,53 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+var passport = require('passport');
+
+
 var router = express.Router();
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+
+router.post('/signup', function(req,res,next){
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+      if(err){
+        res.statusCode = 500;
+        res.setHeader('Content-type','application/json');
+        res.json({err:err})
+
+      }else{
+        passport.authenticate('local')(req,res,() => {
+          res.statusCode = 200;
+          res.setHeader('Content-type','application/json');
+          res.json({success: true, status: 'Registration successful'});
+        });
+      }
+    })
+  });
+
+router.post('/login', passport.authenticate('local'),(req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type','application/json');
+  res.json({success: true, status: 'You are successfully loggin'});
+  
+})
+
+router.get('/logout', (req,res) => {
+  if(req.session){
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }else{
+    var err = new Error('You are not logged in');
+    err.status = 403;
+    next(err);
+  }
+})
 module.exports = router;
